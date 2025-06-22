4c4852215d24d8846ea0c119c0b71cdfe8148212
Le Hoang Thinh <thinhphuongdong3@gmail.com>
2025-06-22 15:38:50 +0700

    test(integration): Add two basic test for the integration test

---
+++ integration-test/test-db-connection.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DEFAULT_PORT=8080
+DEFAULT_ADDRESS=localhost
+
+URL="http://$DEFAULT_ADDRESS:$DEFAULT_PORT/check"
+
+# -w "\n%{http_code}" appends the code on a new line
+response=$(curl -s -w "\n%{http_code}" "$URL")
+body=$(head -n -1 <<<"$response")
+code=$(tail -n1 <<<"$response")
+
+echo "HTTP Status: $code"
+echo "Response Body:"
+echo "$body"
