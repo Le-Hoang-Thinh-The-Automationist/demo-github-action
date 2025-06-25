27c7bb4d4cefcef92dfb25b194e7a5745ce2f74a
Le Hoang Thinh <thinhphuongdong3@gmail.com>
2025-06-25 08:48:06 +0700

    feat(test): Testing

--- integration-test/test-db-connection.bash
+++ integration-test/test-db-connection.bash
@@ -9,6 +9,13 @@ response=$(curl -s -w "\n%{http_code}" "$URL")
 body=$(head -n -1 <<<"$response")
 code=$(tail -n1 <<<"$response")
 
+echo "DEFAULT_ADDRESS: $DEFAULT_ADDRESS"
+echo "DEFAULT_PORT: $DEFAULT_PORT"
+echo "==================="
+
 echo "HTTP Status: $code"
 echo "Response Body:"
 echo "$body"
+
+# For fail purpose
+exit 1
